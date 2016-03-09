31d240c6d1edb38ed5d6f8aa77a38423c82f421c
Marco Bagnasco <admin@hecticmojo.com>
2016-03-09 18:16:25 +0000

    Retrieves data from GitHub API
    - Displaying list of usernames in index.html
    - Using $http service to GET MakersAcademy members from github.com

--- app/controllers.js
+++ app/controllers.js
@@ -1,9 +1,15 @@
 
 myApp.controller('ghUsersCtrl', function($scope, $http) {
+  
   $http({
     method: 'GET',
     url: 'https://api.github.com/orgs/makersacademy/members'
   }).then(function successCallback(response) {
+    $scope.data = response
     
-  })
+  }, function errorCallback(response) {
+    console.log('Error =' + response )
+ });
+
+
 })
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -13,13 +13,13 @@ describe('my app', function() {
   it('should show a list of members on the main page', function() {
     browser.get('index.html');
     var section = $('#user-list')
-    expect(section.getText()).toBe('User 1')
+    expect(section.getText()).toContain('Username')
   });
   
   it('should display a list of users', function() {
     browser.get('index.html');
     var list = $('ul#list')
-    expect(list.getText()).toBe('User 1');
+    expect(list.getText()).toMatch(/Username/);
   });
 
   
