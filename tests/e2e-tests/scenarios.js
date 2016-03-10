c78b210122db196912d473e0be1b985fec2807c4
Marco Bagnasco <admin@hecticmojo.com>
2016-03-10 17:57:52 +0000

    Closes user story 5 - User followers displayed
    - Renamed the controller to ListController
    - Added unit test for controller using $httpBackend service
    - Removed $scope and added alias "listC" for ListController
    - Added number of followers for each user in index

--- tests/e2e-tests/scenarios.js
+++ tests/e2e-tests/scenarios.js
@@ -13,13 +13,13 @@ describe('my app', function() {
   
   it('should show a list of members on the main page', function() {
     browser.get('index.html');
-    var section = $('#user-list');
+    var section = $('section#user-list');
     expect(section.getText()).toContain('Username');
   });
   
   it('should display a list of users', function() {
     browser.get('index.html');
-    var list = $('ul#list');
+    var list = $('section#user-list');
     expect(list.getText()).toMatch(/Username/);
   });
 
@@ -38,10 +38,18 @@ describe('my app', function() {
   it('should return a filtered search', function() {
     browser.get('index.html');
     element(by.model('query')).sendKeys('Adrian')
-    var list = $('ul#list')
+    var list = $('section#user-list')
     expect(list.getText()).not.toMatch(/Bibiana/)
     expect(list.getText()).toMatch(/Adrian/)
   });
+  
+  it('should display the number of followers the user has', function() {
+    browser.get('index.html');
+    var list = $('section#user-list');
+    element(by.model('query')).sendKeys('Adrian')
+    expect(list.getText()).toMatch(/Followers: \d+/);
+  });
+  
 
 });
 
