317d34241438fa288a3686ad3e51f8f93c73a09a
Kait Beaudette <kait@kaitmakesthings.com>
2016-03-10 11:15:13 -0600

    Closes Issue #4 Add User Repos - Added feature test for displaying number of user repos - Displayed number of user repos in list

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -4,8 +4,6 @@
 
 describe('my app', function() {
 
- 
-   
   it('should automatically redirect to /index.html when location hash/fragment is empty', function() {
     browser.get('index.html');
     expect(browser.getLocationAbsUrl()).toMatch("/index.html");
@@ -23,29 +21,32 @@ describe('my app', function() {
     expect(list.getText()).toMatch(/Username/);
   });
 
-<<<<<<< HEAD
   it('should display a profile picture of the user', function() {
     browser.get('index.html');
-    // var image = $('img#10603440');
-    // console.log(image);
     expect(element(by.id('10603440')).isPresent()).toBe(true);
   });
-=======
+
   it('should display a search box', function() {
     browser.get('index.html');
     expect(element(by.id('search')).isPresent()).toBe(true);
   });
 
-  it('should return a filtered search', function($httpBackend) {
-    $httpBackend.whenGET('https://api.github.com/orgs/makersacademy/members').respond('name');
+  it('should return a filtered search', function() {
     browser.get('index.html');
     element(by.model('query')).sendKeys('Adrian')
     var list = $('ul#list')
     expect(list.getText()).not.toMatch(/Bibiana/)
     expect(list.getText()).toMatch(/Adrian/)
-  });
+  });  
 
->>>>>>> master
+  it('should return the number of repos for a user', function() {
+    browser.get('index.html');
+    element(by.model('query')).sendKeys('Adrian')
+    var repos = $('p.repo');
+    var repoNumber = $('span.repoNumber')
+    expect(repos.getText()).toContain("Repos:");
+    expect(repoNumber.getText()).toBeGreaterThan(-1);
+    });  
 });
 
 
