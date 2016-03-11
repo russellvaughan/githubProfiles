733f7d233d1fab3044f47efecbfbaa4ede6eeda5
Marco Bagnasco <admin@hecticmojo.com>
2016-03-11 14:37:27 +0000

    WIP factory and data display

--- app/controllers.js
+++ app/controllers.js
@@ -1,16 +1,48 @@
+myApp.factory('UserListFactory', function($http){
+  var userList = {};
+  
+  userList.getList = function() {
+    
+    return $http.get('https://api.github.com/orgs/makersacademy/members').then(function(response) {
+      this.members = response.data;
+      return this.members;
+    })
+  }
+  
+  userList.followers = function() {
+   
+    return $http.get().then(function(response) {
+      this.followersJson = response;
+      console.log(this.followersJson);
+      return this.followersJson;
+    })
+  }
 
-myApp.controller('ListController', function($http) {
+  return userList;
+});
+
+myApp.controller('ListController', function(UserListFactory) {
   var self = this
   
-  $http({
-    method: 'GET',
-    url: 'https://api.github.com/orgs/makersacademy/members'
-  }).then(function successCallback(response) {
-    self.members = response
+  UserListFactory.getList().then(function(response) {
+    self.userData = response;
+  })
+  
+  UserListFactory.followers().then(function(response) {
+    self.followersData = response;
+    return self.followersData;
+  })
+  
+   self.getLink = function(link) {
+     this.link = link;
+   }
+  
+   // UserListFactory.followers().then(function(response) {
+   //   self.followers = response;
+   //   console.log(self.followers);
+   // }) 
+   
+   
     
-  }, function errorCallback(response) {
-    console.log('Error =' + response )
- });
-
-
+ 
 })
