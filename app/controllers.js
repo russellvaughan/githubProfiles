13c25e788b0002c1dbd10a30a8dfec2f6c1e9a6d
Marco Bagnasco <admin@hecticmojo.com>
2016-03-11 18:21:11 +0000

    controller factory refactoring

--- app/controllers.js
+++ app/controllers.js
@@ -1,48 +1,9 @@
-myApp.factory('UserListFactory', function($http){
-  var userList = {};
-  
-  userList.getList = function() {
-    
-    return $http.get('https://api.github.com/orgs/makersacademy/members').then(function(response) {
-      this.members = response.data;
-      return this.members;
-    })
-  }
-  
-  userList.followers = function() {
-   
-    return $http.get().then(function(response) {
-      this.followersJson = response;
-      console.log(this.followersJson);
-      return this.followersJson;
-    })
-  }
-
-  return userList;
-});
 
 myApp.controller('ListController', function(UserListFactory) {
   var self = this
   
   UserListFactory.getList().then(function(response) {
     self.userData = response;
-  })
-  
-  UserListFactory.followers().then(function(response) {
-    self.followersData = response;
-    return self.followersData;
-  })
-  
-   self.getLink = function(link) {
-     this.link = link;
-   }
-  
-   // UserListFactory.followers().then(function(response) {
-   //   self.followers = response;
-   //   console.log(self.followers);
-   // }) 
-   
-   
-    
+  })    
  
 })
